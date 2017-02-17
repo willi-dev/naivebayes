b782cdf73ac67e720caccc8e62df2aee2fc28d08
willi-dev <willi.ilmukomputer@gmail.com>
2017-02-17 13:29:50 +0700

    first commit

---
+++ src/MainApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author wwww.github.com/willi-dev
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/NaiveBayes.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author www.github.com/willi-dev
+ *
+ */
+public class NaiveBayes {
+	
+	/*
+	 * separateClass
+	 * @param
+	 * void
+	 */
+	public void separateClass(){
+		
+	}
+	
+	
+	
+}
